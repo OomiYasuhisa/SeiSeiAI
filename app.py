1fdc3dd5c112922cc8218b3672500a285eeee87b
逢見泰久 <oomi.yasuhisa.256@gmail.com>
2024-03-10 00:43:06 +0900

    new file:   .gitignore 	new file:   .vscode/launch.json 	new file:   app.py

---
+++ app.py
@@ -0,0 +1,56 @@
+from cgitb import text
+import os
+from urllib import response
+from dotenv import load_dotenv
+import google.generativeai as gemini
+import google.ai.generativelanguage as glm
+import streamlit as st
+
+load_dotenv(".env")
+
+# API key set
+gemini.configure(api_key=os.getenv("API_KEY"))
+
+# Title setting
+st.set_page_config(
+    page_title="セイセイAI",
+    page_icon="😎"
+)
+
+st.title("😎 セイセイAI")
+
+# Session init
+if "chat_session" not in st.session_state :
+    model = gemini.GenerativeModel("gemini-pro")
+    st.session_state["chat_session"] = model.start_chat(history=[
+        glm.Content(role="user", parts=[glm.Part(text="あなたは優秀なAIアシスタントです。3行以内で簡潔に回答してください。")]),
+        glm.Content(role="model", parts=[glm.Part(text="わかりました。")])
+    ])
+    st.session_state["chat_history"] = []
+
+# Show chat log all
+for message in st.session_state["chat_history"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Function after user input and send
+if prompt := st.chat_input("ここに入力してください"):
+
+    # Show user input
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Add user input in chat log
+    st.session_state["chat_history"].append({"role": "user", "content": prompt})
+
+    # Send message for Gemini Pro
+    response = st.session_state["chat_session"].send_message(prompt)
+
+    # Show response
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+
+    # Add responce in chat log
+        st.session_state["chat_history"].append({"role": "assistant", "content": response.text})
+
+
